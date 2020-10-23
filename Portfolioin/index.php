90820422ea69308e8ea00ed138d5c8a506a41db1
John <jonathan.charlet.pro@gmail.com>
2020-10-23 20:06:32 +0200

    Portfolio

--- Portfolioin/index.php
+++ Portfolioin/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>W3.CSS Template</title>
+  <title>Portfolio Jonathan Charlet</title>
   <meta charset="UTF-8">
 
   <link rel="stylesheet"  href="https://www.w3schools.com/w3css/4/w3.css">
